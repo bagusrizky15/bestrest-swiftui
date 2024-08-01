43fdc9711a7eefd980088409104f53b26e216f0e
 Bagus Rizky 🦊 <mohamad_rizky@bca.co.id>
2024-08-01 15:22:27 +0700

    init

---
+++ betterrest/ContentView.swift
@@ -0,0 +1,98 @@
+//
+//  ContentView.swift
+//  betterrest
+//
+//  Created by BBPDEV on 01/08/24.
+//
+
+import SwiftUI
+import CoreML
+
+struct ContentView: View {
+    
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = defaultWakeTime
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
+    
+    var body: some View {
+        NavigationStack {
+                Form {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    Section {
+                        Text("Desired amount of sleep")
+                            .font(.headline)
+                        
+                        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12)
+                    }
+                    Section {
+                        VStack(alignment: .leading) {
+                            Text("Daily coffee intake")
+                                .font(.headline)
+                            Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
+                            DatePicker("Please enter the date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                                .labelsHidden()
+                        }
+                    }
+                    
+                    Section {
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                calculateBedtime()
+                            }) {
+                                Text("Calculate")
+                                    .bold()
+                            }
+                            Spacer()
+                        }
+                    }
+                }
+                .navigationTitle("BetterRest")
+                .alert(alertTitle, isPresented: $showingAlert) {
+                    Button("OK"){}
+                } message: {
+                    Text(alertMessage)
+                }
+                
+        }
+    }
+    
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try BetterRestMLModule(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is ..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime"
+        }
+        
+        showingAlert = true
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ betterrest/betterrestApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterrestApp.swift
+//  betterrest
+//
+//  Created by BBPDEV on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct betterrestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
